e5ff9a6242b8999e544910a87b66f99a5ac72c8c
kallaikrisz <kallaikrisz@gmail.com>
2026-03-18 11:17:20 +0100

    kesz

--- Keresztrejtveny/Program.cs
+++ Keresztrejtveny/Program.cs
@@ -1,1 +1,83 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Kozos;
+//5.feladat
+KeresztrejtvenyRacs aktualis = new KeresztrejtvenyRacs("kr1.txt");
+Console.WriteLine("5. feladat: A keresztrejtvény mérete");
+Console.WriteLine($"\tSorok száma: {aktualis.SorokDb-2}");
+Console.WriteLine($"\tOszlopok száma: {aktualis.OszlopokDb-2}");
+//6.feladat
+for (int i = 0; i < aktualis.SorokDb - 2; i++)
+{
+    for (int j = 0; j < aktualis.OszlopokDb - 2; j++)
+    {
+        if(aktualis.RacsAdatok[i + 1, j + 1]=='-')
+        Console.Write("[]");
+        else Console.Write("##");
+    }
+    Console.WriteLine();
+}
+//7.feladat
+int leghosszabb = 0;
+for (int i = 1; i < aktualis.OszlopokDb - 2; i++)
+{
+    for (int j = 1; j < aktualis.SorokDb - 2; j++)
+    {
+        if (aktualis.RacsAdatok[j + 1, i + 1] == '-')
+        {
+            int hossza = 0;
+            while (j < aktualis.SorokDb - 2 && aktualis.RacsAdatok[j + 1, i + 1] == '-')
+            {
+                hossza++;
+                j++;
+            }
+            if (hossza > leghosszabb)
+                leghosszabb = hossza;
+        }
+    }
+}
+Console.WriteLine($"7. feladat: A leghoszabb függőleges: {leghosszabb} karakter");
+//8.feladat
+Console.WriteLine("8. feladat: Vízszintes szavak statisztikája");
+List<int> stat = new List<int>();
+for (int i = 1; i < aktualis.SorokDb - 1; i++)
+{
+    int j = 1;
+    while (j < aktualis.OszlopokDb - 1)
+    {
+        if (aktualis.RacsAdatok[i, j] == '-')
+        {
+            int hossz = 0;
+            while (j < aktualis.OszlopokDb - 1 && aktualis.RacsAdatok[i, j] == '-')
+            {
+                hossz++;
+                j++;
+            }
+            if (hossz >= 2) stat.Add(hossz);
+        }
+        else
+        {
+            j++;
+        }
+    }
+}
+for (int i = 2; i < aktualis.OszlopokDb - 2; i++)
+{
+    int db = stat.Count(x => x == i);
+    if (db > 0) Console.WriteLine($"\t{i} betűs: {db} darab");
+}
+//9.feladat
+Console.WriteLine("9. feladat: A keresztrejtvény számokkal");
+aktualis.Sorszamoz();
+for (int i = 1; i < aktualis.RacsAdatok.GetLength(0) - 1; i++)
+{
+    string sor = "";
+    for (int j = 1; j < aktualis.RacsAdatok.GetLength(1) - 1; j++)
+    {
+        char c = aktualis.RacsAdatok[i, j];
+        int sz = aktualis.SorszamokAdatok[i, j];
+        if (c == '#')sor += "##";
+        else if (sz > 0)sor += sz.ToString("D2");
+        else sor += "[]";
+    }
+    Console.WriteLine(sor);
+}
+
--- Kozos/KeresztrejtvenyRacs.cs
+++ Kozos/KeresztrejtvenyRacs.cs
@@ -9,6 +9,8 @@
 
         public int OszlopokDb => Racs?.GetLength(1) ?? 0;
         public int SorokDb => Racs.GetLength(0);
+        public char[,] RacsAdatok => Racs;
+        public int[,] SorszamokAdatok => Sorszamok;
 
         public KeresztrejtvenyRacs(string forras)
         {
@@ -25,15 +27,38 @@
         {
             int sorDb = Adatsorok.Count;
             int oszlopDb = Adatsorok[0].Length;
-
             Racs = new char[sorDb+2, oszlopDb+2];
             Sorszamok = new int[sorDb+2, oszlopDb+2];
-
+            for (int i = 0; i < sorDb + 2; i++)
+            {
+                for (int j = 0; j < oszlopDb + 2; j++)
+                {
+                    Racs[i, j] = '#';
+                }
+            }
             for (int i = 0; i < sorDb; i++)
             {
                 for (int j = 0; j < oszlopDb; j++)
                 {
                     Racs[i+1, j+1] = Adatsorok[i][j];
+                }
+            }
+        }
+        public void Sorszamoz()
+        {
+            int sorszam = 1;
+            for (int i = 1; i < Racs.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j < Racs.GetLength(1) - 1; j++)
+                {
+                    if (Racs[i, j] != '-') continue;
+                    bool vizszintesKezdet =Racs[i, j - 1] == '#' && Racs[i, j + 1] == '-'; 
+                    bool fuggolegesKezdet =Racs[i - 1, j] == '#' && Racs[i + 1, j] == '-';
+                    if (vizszintesKezdet || fuggolegesKezdet)
+                    {
+                        Sorszamok[i, j] = sorszam;
+                        sorszam++;
+                    }
                 }
             }
         }
